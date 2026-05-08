eca019c471c5c61c632ee856c4791606acffc28c
stefano-rago <stefa.rago@gmail.com>
2026-05-07 21:29:36 -0300

    Conexión exitosa entre ProductsController y Models

---
+++ Products.API/Controllers/ProductsControllers.cs.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Products.API_.Models;
+
+namespace Products.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // Esto es una lista temporal para probar que la API funciona
+        private static List<Product> _products = new List<Product>();
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return _products;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Product newProduct)
+        {
+            newProduct.Id = Guid.NewGuid();
+            newProduct.FechaCreacion = DateTime.Now;
+            _products.Add(newProduct);
+            return Ok(newProduct);
+        }
+    }
+}
--- Products.API/obj/Debug/net8.0/Products.API..MvcApplicationPartsAssemblyInfo.cs
+++ Products.API/obj/Debug/net8.0/Products.API..MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Products.API.")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22930f479e6203e6ee169dedce23f3be30c2f637")]
-[assembly: System.Reflection.AssemblyProductAttribute("Products.API.")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Products.API.")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.OpenApi")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
