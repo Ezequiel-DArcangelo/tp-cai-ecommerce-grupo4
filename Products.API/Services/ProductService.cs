f10823507fb99337343a78e662636c52938eb56c
stefano-rago <stefa.rago@gmail.com>
2026-05-18 21:23:58 -0300

    Migración de la lista de productos y método GetAll al servicio
    Pasé la lista temporal de productos al ProductService y armé el método GetAll.

--- Products.API/Services/ProductService.cs
+++ Products.API/Services/ProductService.cs
@@ -1,6 +1,23 @@
-﻿namespace Products.API_.Services
+﻿using Products.API_.Models;
+
+namespace Products.API.Services
+
 {
     public class ProductService
+
     {
+        // Movimos la lista temporal de productos hacia acá 
+        private static readonly List<Product> _products = new List<Product>();
+
+        // Método para obtener todos los productos
+        public IEnumerable<Product> GetAll()
+
+        {
+          
+          return _products;
+        
+        }
+
     }
+
 }
