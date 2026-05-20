a58f88a7ecdb27e92f3fd674468f9a8e5ef88b09
Ezequiel D'Arcangelo <56dx37234543@campus.economicas.uba.ar>
2026-05-19 23:13:11 -0300

    Agregar excepciones de dominio para manejo de errores
    Se crean NotFoundException, BusinessRuleException y ValidationException
       en la carpeta Exceptions.

---
+++ Users.API/Users.API/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Users.API.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public string ErrorCode { get; set; }
+
+        public NotFoundException(string errorCode, string message) : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
---
+++ Users.API/Users.API/Exceptions/ValidationException.cs
@@ -0,0 +1,12 @@
+﻿namespace Users.API.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public string ErrorCode { get; set; }
+
+        public ValidationException(string errorCode, string message) : base(message)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
