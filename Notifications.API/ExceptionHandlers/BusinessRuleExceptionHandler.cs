b02d0e87469e6e96b5c729a39344a559a8d8b8b7
stefano-rago <stefa.rago@gmail.com>
2026-06-10 16:54:09 -0300

    Implementación del BusinessRuleExceptionHandler
    Agregué el manejador BusinessRuleExceptionHandler en la carpeta ExceptionHandlers. Se encarga de interceptar las excepciones de tipo BusinessRuleException para devolver un estado HTTP 400 Bad Request junto con el JSON formateado.

---
+++ Notifications.API/ExceptionHandlers/BusinessRuleExceptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Notifications.API.Exceptions;
+
+namespace Notifications.API.ExceptionHandlers
+{
+    public class BusinessRuleExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        {
+            if (exception is not BusinessRuleException ex) return false;
+            
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsJsonAsync(new
+            {
+                type = "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+                title = "Bad Request",
+                status = 400,
+                detail = "La solicitud no cumple con las reglas de negocio.",
+                instance = context.Request.Path.Value,
+                errorCode = ex.ErrorCode,
+                errorMessage = ex.Message
+            }, cancellationToken);
+
+            return true;
+        }
+    }
+}
--- Notifications.API/obj/Debug/net8.0/Notifications.API.AssemblyInfo.cs
+++ Notifications.API/obj/Debug/net8.0/Notifications.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notifications.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c88c10bc9cb4c06a0507ef1a11d19bdb938872")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7452de890f93c05c70904086927bd4a557ca15b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notifications.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notifications.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
