fed7bb72c07de70181a07b6bc93320fed49cbdd8
stefano-rago <stefa.rago@gmail.com>
2026-06-10 17:24:28 -0300

    Implementación de GlobalExceptionHandler para errores genéricos
    Agregué el manejador de contingencia final en la carpeta ExceptionHandlers que se encarga de capturar cualquier excepción imprevista no controlada para retornar un estado HTTP 500.

---
+++ Notifications.API/ExceptionHandlers/GlobalExceptionHandler.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace Notifications.API.ExceptionHandlers
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext context, Exception exception, CancellationToken cancellationToken)
+        {
+            // Este manejador no lleva IF porque se encarga de manejar cualquier excepción no manejada por los otros handlers.
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(new
+            {
+                type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+                title = "Internal Server Error",
+                status = 500,
+                detail = "Ocurrió un error inesperado en el servidor.",
+                instance = context.Request.Path.Value,
+                errorMessage = exception.Message
+            }, cancellationToken);
+
+            return true;
+        }
+    }
+}
--- Notifications.API/obj/Debug/net8.0/Notifications.API.AssemblyInfo.cs
+++ Notifications.API/obj/Debug/net8.0/Notifications.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notifications.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7452de890f93c05c70904086927bd4a557ca15b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b02d0e87469e6e96b5c729a39344a559a8d8b8b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notifications.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notifications.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
