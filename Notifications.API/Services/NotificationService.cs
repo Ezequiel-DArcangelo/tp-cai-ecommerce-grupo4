49a9252002b1c666a71deca1f3e808ed2b10360f
stefano-rago <stefa.rago@gmail.com>
2026-06-10 21:13:13 -0300

    Implementación del método GET de listado en NotificationService
    Filtra el historial por ID de usuario y maneja el error de negocio NTF-003 en caso de no registrar alertas previas.

--- Notifications.API/Services/NotificationService.cs
+++ Notifications.API/Services/NotificationService.cs
@@ -1,6 +1,7 @@
 ﻿using Notifications.API.DTOs;
 using Notifications.API.Exceptions;
 using Notifications.API.Models;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Notifications.API.Services;
 
@@ -15,13 +16,13 @@ public class NotificationService
         Guid.Parse("a1b2c3d4-0000-0000-0000-111122223333")
     };
 
-    // 1. Lógica para registrar (POST)
+    // 1. Lógica para registrar/enviar (POST)
     public NotificationResponse SendNotification(CreateNotificationRequest request)
-        {
+    {
         // Validación de existencia de usuario (Simulación del llamado a la API de Usuarios)
         if (!_existingUsersMock.Contains(request.UsuarioId))
         {
-            throw new NotFoundException("NTF-001","El usuario destinatario no fue encontrado.");
+            throw new NotFoundException("NTF-001", "El usuario destinatario no fue encontrado.");
         }
 
         // Si el usuario efectivamente existe, mapeamos el DTO al Modelo de dominio para guardarlo
@@ -34,7 +35,7 @@ public class NotificationService
 
         // Simulación de guardado en base de datos (en este caso, una lista en memoria)
         _notificationsDb.Add(newNotification);
-     
+
         // Retornar respuesta
         return new NotificationResponse
         {
@@ -45,4 +46,29 @@ public class NotificationService
             FechaEnvio = newNotification.FechaEnvio
         };
     }
+
+        // 2.Lógica para listar por usuario (GET)
+        public IEnumerable<NotificationResponse> GetNotificationsByUserId(Guid userId)
+        {
+           // Buscamos en la lista del sistema las notificaciones asociadas al ID del usuario
+           var userNotifications = _notificationsDb.Where(n => n.UsuarioId == userId).ToList();
+
+           // Si el usuario no tiene ninguna notificación registrada (NTF-003)
+           if (!userNotifications.Any())
+           {
+             throw new NotFoundException("NTF-003", "No se encontraron notificaciones para el usuario especificado.");
+           }
+
+           // Mapeamos cada notificación encontrada a un DTO de respuesta
+           return userNotifications.Select(n => new NotificationResponse
+           {
+             Id = n.Id,
+             UsuarioId = n.UsuarioId,
+             Mensaje = n.Mensaje,
+             Tipo = n.Tipo,
+             Estado = n.Estado,
+             FechaEnvio = n.FechaEnvio
+            });
+
+        }
 }
